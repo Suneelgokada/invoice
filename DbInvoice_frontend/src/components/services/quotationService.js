a3c241db0a333079a533b75293b45e8921590983
suneel <suneelgokada@gmail.com>
2026-03-10 14:11:58 +0530

    ok

--- DbInvoice_frontend/src/components/services/quotationService.js
+++ DbInvoice_frontend/src/components/services/quotationService.js
@@ -3,7 +3,7 @@ import api from "../../api/axiosClient";
 export const generateQuotationPDF = async (content) => {
 
   const res = await api.post(
-    "/api/admin/generate-document",
+    "/api/quotation/generate-document",
     { content },
     { responseType: "blob" }
   );
@@ -11,12 +11,9 @@ export const generateQuotationPDF = async (content) => {
   const url = window.URL.createObjectURL(new Blob([res.data]));
 
   const link = document.createElement("a");
-
   link.href = url;
-
-  link.setAttribute("download", "quotation.pdf");
+  link.download = "quotation.pdf";
 
   document.body.appendChild(link);
-
   link.click();
 };
