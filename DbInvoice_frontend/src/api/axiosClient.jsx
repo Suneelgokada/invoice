e0d850529673fefe996397c821f927819630d032
suneel <suneelgokada@gmail.com>
2026-03-14 11:19:27 +0530

    cors

--- DbInvoice_frontend/src/api/axiosClient.jsx
+++ DbInvoice_frontend/src/api/axiosClient.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://invoice-dbinvoice-backend.onrender.com";
+    : "https://invoice-backend-um3n.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
